e2cd1ffab4b0c08a41987693b97ad709f8fc6b33
Josué Martínez <79375365+JosueMartinez-CR@users.noreply.github.com>
2022-07-29 11:18:53 -0600

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@
 import mongoose, { connect } from 'mongoose'
 
 export async function startConnection() {
-   await connect('mongodb+srv://jota:jota1234@cluster0.4ft9hwi.mongodb.net/?retryWrites=true&w=majority');
+   await connect('MONGO URL HERE');
    console.log("DATABASE CONNECTED",
    );
 }

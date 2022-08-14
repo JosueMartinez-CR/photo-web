223934ea7896a596a2d1bb2f8a66c1e92f5a7761
Josue M <josuemartinez.cr@gmail.com>
2022-08-14 10:59:39 -0600

    asegurando datos del programa (variables de entorno)

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,8 @@
 
-
 import mongoose, { connect } from 'mongoose'
 
 export async function startConnection() {
-   await connect('mongodb+srv://jota:jota1234@cluster0.4ft9hwi.mongodb.net/?retryWrites=true&w=majority');
+   await connect(process.env.DATABASE_CONNECTION || "DATABASE");
    console.log("DATABASE CONNECTED",
    );
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 
 //This file is responsible for starting the application.
+import dotenv from 'dotenv';
+dotenv.config();               //variables de entorno para solo enviar parametros
 
 import app from './app';
 import {startConnection} from './database';

0b057d1a9c74b1e1bf2bbd1f7e61776a71468218
Josue M <josuemartinez.cr@gmail.com>
2022-08-14 11:55:21 -0600

    Token Creado

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -11,8 +11,18 @@ export async function loggin (req:Request, res:Response){
     console.log(data?.password, req.body.password);
     if (data){
         const passwordValido = bcrypt.compareSync(req.body.password,data.password);
-        if(!passwordValido){return res.status(400).json({error:'ok', msj:'Usuario o contraseña incorrecta.'})
-                
+        if(!passwordValido){return res.status(400).json('Usuario o contraseña incorrecta.')
+        
+        //token
+
+        const token: string = jwt.sign(
+            {_id: data?._id, 
+            email: data?.email, 
+            password: data?.password},
+            process.env.TOKEN_SECRET||"SECRET",
+            {expiresIn: '24h'
+                    });
+
     }else{
         return res.json("Correct password!")
     }
